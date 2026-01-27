3b11d092fa7189a20fc2468a670da07a23f31a55
SE1 <xdxd0000000000@gmail.com>
2026-01-27 15:19:01 +0700

    upv7

--- app/src/main/java/com/earth/earth55/mapFragment.java
+++ app/src/main/java/com/earth/earth55/mapFragment.java
@@ -1,64 +1,97 @@
 package com.earth.earth55;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link mapFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.Objects;
+
+
 public class mapFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private int locationRequestCode = 1000;
+    private Button button;
+    private SupportMapFragment mapFragment;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private double[] clat = {0};
+    private double[] clng = {0};
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public mapFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment mapFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static mapFragment newInstance(String param1, String param2) {
-        mapFragment fragment = new mapFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        button = (Button) view.findViewById(R.id.button);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity());
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        return view;
     }
+    private void Get_Current_location() {
+        if (ActivityCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getActivity(),
+                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            clat[0] = location.getLatitude();
+                            clng[0] = location.getLongitude();
+                        }
+                    }
+                });
+    }
+    private OnMapReadyCallback callbackactive = new OnMapReadyCallback() {
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            Get_Current_location();
+            googleMap.clear();
+            String strtitle = "Your Location";
+            String strsnippet = "I am Here at " + clat[0] + ", " + clng[0];
+            LatLng cposition = new LatLng(clat[0], clng[0]);
+
+            MarkerOptions options = new MarkerOptions()
+                    .position(new LatLng(clat[0], clng[0]))
+                    .title(strtitle)
+                    .snippet(strsnippet)
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+            googleMap.addMarker(options);
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(cposition, 12));
+        }
+    };
 }
